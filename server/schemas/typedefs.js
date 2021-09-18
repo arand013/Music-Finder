33551efc49c407a97fdd7054de81a35dd85ed9c7
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-18 01:50:09 -0700

    server: schemas: typeDefs.js started

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,7 @@
+// import the gql tagged template function (they are an advanced use if template literals)
+const { gql } = require("apollo-server-express");
+
+
+// export the typeDefs
+module.exports = typeDefs;
+

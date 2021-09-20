7e35cdacbb69a4f77762062d8dd7b23eb9d6aa69
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-19 23:06:48 -0700

    client: utils: localStorage - accepted change to local Storage

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-export const getSavedBookIds = () => {
-    const savedBookIds = localStorage.getItem('saved_books')
-      ? JSON.parse(localStorage.getItem('saved_books'))
-      : [];
-  
-    return savedBookIds;
-  };
-  
-  export const saveBookIds = (bookIdArr) => {
-    if (bookIdArr.length) {
-      localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
-    } else {
-      localStorage.removeItem('saved_books');
-    }
-  };
-  
-  export const removeBookId = (bookId) => {
-    const savedBookIds = localStorage.getItem('saved_books')
-      ? JSON.parse(localStorage.getItem('saved_books'))
-      : null;
-  
-    if (!savedBookIds) {
-      return false;
-    }
-  
-    const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
-    localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
-  
-    return true;
-  };
-=======
 export const getSavedSongsIds = () => {
     const savedSongIds = localStorage.getItem('saved_songs')
       ? JSON.parse(localStorage.getItem('saved_songs'))
@@ -60,4 +28,3 @@ export const getSavedSongsIds = () => {
   
     return true;
   };
->>>>>>> develop
